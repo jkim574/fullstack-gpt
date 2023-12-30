0750e80165bb7bdd44e50bba54b358e8ca62253a
jkim574 <kjhcommit574@gmail.com>
2023-12-30 01:35:46 -0800

    DocumentGPT page added

---
+++ Home.py
@@ -0,0 +1,26 @@
+# Create a Home for all applications
+# building portfolio for all GPT apps 
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🫧",
+)
+
+st.markdown(
+    """
+    # Hello!
+    
+    Welcome to my FullstackGPT Portfolio!
+    
+    Here are the apps I made:
+    
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+    """
+)
